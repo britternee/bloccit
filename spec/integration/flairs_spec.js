8d1250c26a0882ed2810b778156bfc0e9ea7288a
Brittany Miller <britmill@gmail.com>
2018-10-09 16:37:57 -0600

    post resource 2 assignment done

--- spec/integration/flairs_spec.js
+++ spec/integration/flairs_spec.js
@@ -17,27 +17,27 @@ describe("routes : flairs", () => {
             Topic.create({
                 title: "Winter Games",
                 description: "Post your Winter Games stories."
-            })
-            .then((topic) => {
-                this.topic = topic;
+                })
+                    .then((topic) => {
+                    this.topic = topic;
 
-                Flair.create({
-                name: "SkiBall",
-                color: "Blue",
-                topicId: this.topic.id
-            })
-            .then((flair) => {
-                this.flair = flair;
-                done();
-            })
-            .catch((err) => {
-                console.log(err);
-                done();
+                    Flair.create({
+                        name: "SkiBall",
+                        color: "Blue",
+                        topicId: this.topic.id
+                    })
+                    .then((flair) => {
+                    this.flair = flair;
+                    done();
+                    })
+                    .catch((err) => {
+                    console.log(err);
+                    done();
+                });
             });
         });
     });
-    });
-});
+
 
     describe("GET /topics/:topicId/flairs/new", () => {
 
@@ -46,6 +46,111 @@ describe("routes : flairs", () => {
                 expect(err).toBeNull();
                 expect(body).toContain("New Flair");
                 done();
+            });
+        });
+    });
+
+    describe("POST /topics/:topicId/flairs/create", () => {
+
+        it("should create a new flair and redirect", (done) => {
+            const options = {
+                url: `${base}/${this.topic.id}/flairs/create`,
+                form: {
+                name: "SkiBall",
+                color: "Blue"
+                }
+            };
+            request.post(options,
+                (err, res, body) => {
+                    Flair.findOne({where: {name: "SkiBall"}})
+                    .then((flair) => {
+                        expect(flair).not.toBeNull();
+                        expect(flair.name).toBe("SkiBall");
+                        expect(flair.color).toBe("Blue");
+                        expect(flair.topicId).not.toBeNull();
+                        done();
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                        done();
+                    });
+            });
+        });
+    });
+
+    describe("GET /topics/:topicId/flairs/:id", () => {
+
+        it("should render a view with the selected flair", (done) => {
+        request.get(`${base}/${this.topic.id}/flairs/${this.flair.id}`, (err, res, body) => {
+            expect(err).toBeNull();
+            expect(body).toContain("SkiBall");
+            done();
+            });
+        })
+    });
+
+    describe("POST /topics/:topicId/flairs/:id/destroy", () => {
+
+        it("should delete the flair with the associated ID", (done) => {
+            expect(this.flair.id).toBe(1);
+            request.post(`${base}/${this.topic.id}/flairs/${this.flair.id}/destroy`, (err, res, body) => {
+                Flair.findById(1)
+                    .then((flair) => {
+                        expect(err).toBeNull();
+                        expect(flair).toBeNull();
+                        done();
+                })
+            });
+        });
+    });
+
+    describe("GET /topics/:topicId/flairs/:id/edit", () => {
+
+        it("should render a view with an edit flair form", (done) => {
+            request.get(`${base}/${this.topic.id}/flairs/${this.flair.id}/edit`, (err, res, body) => {
+                expect(err).toBeNull();
+                expect(body).toContain("Edit Flair");
+                expect(body).toContain("SkiBall");
+                done();
+            });
+        });
+    });
+
+    describe("POST /topics/:topicId/flairs/:id/update", () => {
+
+        it("should return a status code 302", (done) => {
+            request.post({
+                url: `${base}/${this.topic.id}/flairs/${this.flair.id}/update`,
+                form: {
+                    name: "SkiBall",
+                    color: "Blue"
+                }
+            }, (err, res, body) => {
+                expect(res.statusCode).toBe(302);
+                done();
+            });
+        });
+
+        it("should update the flair with the given values", (done) => {
+            const options = {
+                url: `${base}/${this.topic.id}/flair/${this.flair.id}/update`,
+                form: {
+                    name: "SkiBall"
+                    }
+                };
+                request.post(options,
+                    (err, res, body) => {
+
+                    expect(err).toBeNull();
+
+                    Flair.findOne({
+                        where: {id: this.flair.id}
+                    })
+                .then((flair) => {
+                    expect(flair.name).toBe("SkiBall");
+                    done();
+                });
+            });
         });
     });
 });
